add2112cd43ebd1dab229196cf10f55308da1db4
olivergeith <oliver.geith@gmx.net>
2013-05-24 19:46:22 +0200

    Box2 Battery now is xxhdpi!

--- src/de/og/batterycreator/creators/batt/Box2Creator.java
+++ src/de/og/batterycreator/creators/batt/Box2Creator.java
@@ -1,8 +1,11 @@
 package de.og.batterycreator.creators.batt;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GradientPaint;
 import java.awt.Graphics2D;
+import java.awt.LinearGradientPaint;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 import de.og.batterycreator.cfg.RomSettings;
@@ -13,8 +16,9 @@ public class Box2Creator extends AbstractIconCreator {
 
 	public Box2Creator(final RomSettings romSettings) {
 		super(romSettings);
+		settings.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 26));
 		settings.setFontXOffset(-1);
-		settings.setStrokewidth(1);
+		settings.setStrokewidth(2);
 		settings.setBattGradient(true);
 		settings.setUseGradiantForMediumColor(true);
 		settings.setUseGradiantForNormalColor(false);
@@ -33,7 +37,17 @@ public class Box2Creator extends AbstractIconCreator {
 	}
 
 	@Override
+	public boolean supportsLinearGradient() {
+		return true;
+	}
+
+	@Override
 	public boolean supportsNoBg() {
+		return true;
+	}
+
+	@Override
+	public boolean isNativeXXHDPI() {
 		return true;
 	}
 
@@ -44,42 +58,51 @@ public class Box2Creator extends AbstractIconCreator {
 	 */
 	@Override
 	public ImageIcon createImage(final int percentage, final boolean charge) {
-
+		final int imgWidth = 54;
+		final int imgHeight = 54;
 		// Create a graphics contents on the buffered image
-		BufferedImage img = new BufferedImage(40, 40, BufferedImage.TYPE_INT_ARGB);
+		BufferedImage img = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_INT_ARGB);
 		final Graphics2D g2d = initGrafics2D(img);
 
 		g2d.setColor(settings.getIconColorInActiv().brighter());
 		final int w = settings.getStrokewidth();
-		g2d.fillRect(0, 0, 40, w);
-		g2d.fillRect(0, 40 - w, 40, w);
-		g2d.fillRect(0, 0, w, 40);
-		g2d.fillRect(40 - w, 0, w, 40);
+		g2d.fillRect(0, 0, imgWidth, w);
+		g2d.fillRect(0, imgHeight - w, imgHeight, w);
+		g2d.fillRect(0, 0, w, imgHeight);
+		g2d.fillRect(imgWidth - w, 0, w, imgHeight);
 
 		if (!settings.isNoBG()) {
 			if (settings.isBattGradient()) {
 				final Color col1 = settings.getIconColorInActiv();
 				final Color col2 = getBattGardientSecondColor(col1);
-				final GradientPaint gradientFill = new GradientPaint(w, 5, col2, 40 - w - w, 40, col1);
+				final GradientPaint gradientFill = new GradientPaint(w, w, col2, imgWidth - w, imgHeight - w, col1);
 				g2d.setPaint(gradientFill);
 			} else {
 				g2d.setColor(settings.getIconColorInActiv());
 			}
-			g2d.fillRect(w, w, 40 - w - w, 40 - w - w); // Inner Battery
+			g2d.fillRect(w, w, imgWidth - w - w, imgHeight - w - w); // Inner
+																		// Battery
 		}
-		int h = Math.round((40f - w - w - 2) / 100f * percentage);
+		int h = Math.round((imgHeight - w - w - 2) / 100f * percentage);
 		if (h < 2)
 			h = 2;
 
 		if (settings.isBattGradient()) {
-			final Color col1 = settings.getActivIconColor(percentage, charge);
-			final Color col2 = getBattGardientSecondColor(col1);
-			final GradientPaint gradientFill = new GradientPaint(w, 5, col1, 40 - w - w, 5, col2);
-			g2d.setPaint(gradientFill);
+			if (settings.isLinearGradient() && !charge) {
+				final Point2D start = new Point2D.Float(w, imgHeight - w);
+				final Point2D end = new Point2D.Float(w, w);
+				final LinearGradientPaint gradientFill = settings.createLinearGradientPaint(start, end);
+				g2d.setPaint(gradientFill);
+			} else {
+				final Color col1 = settings.getActivIconColor(percentage, charge);
+				final Color col2 = getBattGardientSecondColor(col1);
+				final GradientPaint gradientFill = new GradientPaint(w, w, col1, imgWidth - w, w, col2);
+				g2d.setPaint(gradientFill);
+			}
 		} else {
 			g2d.setColor(settings.getActivIconColor(percentage, charge));
 		}
-		g2d.fillRect(w + 1, 40 - w - 1 - h, 40 - w - w - 2, h);
+		g2d.fillRect(w + 1, imgHeight - w - 1 - h, imgWidth - w - w - 2, h);
 
 		g2d.setPaintMode();
 		drawPercentage(g2d, percentage, charge, img);
