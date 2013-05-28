76955ee25a169abea453b16e1fd643e2a7260c35
olivergeith <oliver.geith@gmx.net>
2013-05-28 16:52:25 +0200

    More font sizes new Packmanrom presets for i747 S3 for all battery styles

--- src/de/og/batterycreator/gui/cfg/BattSettingsPanel.java
+++ src/de/og/batterycreator/gui/cfg/BattSettingsPanel.java
@@ -36,7 +36,8 @@ public class BattSettingsPanel extends SettingsPanel {
 
 	private BattSettings				settings;
 	private final String				fontSizes[]					= {
-			"12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32"
+			"12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35",
+			"36", "38", "39", "40", "41", "42", "43", "44", "45"
 																	};
 
 	private final JColorSelectButton	fontColor					= new JColorSelectButton("Main Color", "Color when normal battery-level");
